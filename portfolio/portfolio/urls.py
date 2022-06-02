10149b87df03bc37afcdfa78bb2e2decfee86859
berat-ademi <berat.ademi@gmail.com>
2022-06-02 20:59:58 +0200

    updated DEBUG mode

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
-if settings.DEBUG:
+if settings.DEBUG == True:
     urlpatterns = [
         path('admin/', admin.site.urls),
         path('',include('portfolioapp.urls')),
